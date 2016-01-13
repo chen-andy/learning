50650d9200b21624efde3378989179e2a4ffd956
chen-andy <andyic@gmail.com>
2016-01-12 19:14:32 -0800

    commit 1

---
+++ WebApplication1/Default.aspx.vb
@@ -0,0 +1,16 @@
+﻿Public Class _Default
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+
+        If (Page.IsPostBack = False) Then
+            Calendar1.SelectedDate = Now
+            Label1.Text = "Pick a selected date"
+        End If
+    End Sub
+
+    Private Sub Calendar1_SelectionChanged(sender As Object, e As EventArgs) Handles Calendar1.SelectionChanged
+        Label1.Text = "You selected: " & Calendar1.SelectedDate.ToShortDateString()
+    End Sub
+
+End Class
---
+++ WebApplication1/WebForm2.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="WebForm2.aspx.vb" Inherits="WebApplication1.WebForm2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="FeaturedContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
